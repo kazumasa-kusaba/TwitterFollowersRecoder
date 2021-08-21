ad01ac1495baeda7b596bddce804c718f5c26016
Kazumasa Kusaba <kazumasa@mail.kusaba.xyz>
2021-08-21 23:21:26 +0900

    add update_csv_file method

--- utils/file_manager.py
+++ utils/file_manager.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
-import sys
-import logging
+import os
 import json
+import csv
 
 class FileManager():
-    def __init__(self, logging_level):
-        log_handler = logging.StreamHandler(sys.stdout)
-        log_handler.setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s'))
-        self.logger = logging.getLogger(__name__)
-        self.logger.addHandler(log_handler)
-        self.logger.setLevel(logging_level)
-    
     def get_json_dict_from_json(self, json_file_path):
         data = self.__read_file(json_file_path)
         json_dicts = json.loads(data)
         return json_dicts
+
+    def update_csv_file(self, directory_path, screen_name, timestamp, friends_count, followers_count):
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+        with open(os.path.join(directory_path, screen_name + ".csv"), "a", encoding="utf-8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([timestamp, friends_count, followers_count])
     
     def __read_file(self, file_path):
         with open(file_path, "r", encoding="utf8") as f:
